e522413bd4abc804e69e757b85eeefebe1172d4b
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-12-05 05:02:15 +0000

    added a style sheet

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    var name = "Damien Robinson";
-    $scope.title = "Damien Robinson";
+    $scope.title = "KnowledgeFlux";
     $scope.keywords = "memes";
 });
