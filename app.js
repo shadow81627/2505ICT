4bd8c809936362b9440e0cdab121ceba298cc753
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-12-05 04:18:16 +0000

    added data binding

---
+++ app.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    var name = "Damien Robinson";
+    $scope.title = "Damien Robinson";
+    $scope.keywords = "memes";
+});
