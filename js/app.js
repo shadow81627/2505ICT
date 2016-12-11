3f815e9e3f7d27a4030defaede90aff6e2f0adf5
Damien Robinson <damien.robinson@griffithuni.edu.au>
2016-12-11 10:45:51 +0000

    I have no idea what I chnaged and im too tired to find out

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ var app = angular.module("app", ["ngRoute"]);
 app.controller('myCtrl', function($scope, $route, $routeParams, $location) {
     $scope.title = "KnowledgeFlux";
     $scope.keywords = "KnowledgeFlux";
+    $scope.photos = flickr.photos;
 });
--- js/view.js
+++ js/view.js
@@ -9,7 +9,16 @@ view.displayThumb = function(result) {
         
     }
     $("#thumbnails").html(htmlStr);
+    
+    var htmlStr = '<div class="card"><div class="card-block text-xs-center"><h4 class="card-title">Create a Post</h4><i class="fa fa-plus-square-o fa-5x" aria-hidden="true"></i> <form class="form-inline float-xs-right"><input class="form-control" type="text" placeholder="Write you post here"><button class="btn btn-outline-success" type="submit">Create</button></form></div></div>';
+    for (var i = 4; i < 8; i++){
+
+        htmlStr += '<div class="card"><img class="card-img-top img-fluid" src="' + result[i].thumbnail +  '" alt="' + result[i].description + '"><div class="card-block"><h4 class="card-title">' + result[i].description + '</h4></div></div>'
+        
+    }
+    $("#thumbnails2").html(htmlStr);
 }
+
 
 
 view.displayUser = function(username){
